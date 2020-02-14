1c29ce7308ab58f12c918b0c31fe4a43756cb939
mark churcher <mark.churcher@montageinteractive.com>
2020-02-14 15:10:33 +1300

    add basename to react router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/burger-builder'>
         <App />
     </BrowserRouter>
     , document.getElementById('root'));
